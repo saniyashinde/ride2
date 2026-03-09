6e8649f9bd9a96e689cf09a1d3cf4e9059144a7b
saniya shinde <shindesaniya06@gmail.com>
2026-03-10 00:18:31 +0530

    your message here

--- backend/routes/ownerRoutes.js
+++ backend/routes/ownerRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Driver = require("../models/Driver");
-const User = require("../models/user");
+const User = require("../models/User");
 const Ride = require("../models/Ride");
 
 /* ===============================
